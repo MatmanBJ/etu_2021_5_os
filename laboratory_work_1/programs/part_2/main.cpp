1c2e7e3017f368fd2cde5cae0b3495a62845d3e0
MatmanBJ <matmanbj@gmail.com>
2022-01-09 19:30:13 +0300

    lab_1 pt_2 v 1_6

--- laboratory_work_1/programs/part_2/main.cpp
+++ laboratory_work_1/programs/part_2/main.cpp
@@ -9,8 +9,6 @@
 
 using namespace std;
 
-//void copyFile(HANDLE firstHandle, HANDLE secondHandle, DWORD blockSize, int operationsCount);
-
 // õýíäëû íà ôàéëû, ó÷àâñòâóþùèå â îïåðàöèè êîïèðîâàíèÿ
 HANDLE firstHandle;
 HANDLE secondHandle;
@@ -18,80 +16,32 @@ HANDLE secondHandle;
 LARGE_INTEGER shiftRead;
 LARGE_INTEGER shiftWrite;
 
-
-
-
-
 int LocalFileGenerator (string localOldFilePath, unsigned long long localBytesRequest);
 void CopyPaste (string localOldFilePath, string localNewFilePath);
 DWORD PreparingCopyPaste(string localOldFilePath, string localNewFilePath, unsigned long long localBlockSize, unsigned long long localOverlappedIOSize);
-DWORD LocalCopyPaste (HANDLE in, HANDLE out, unsigned long long fileSize, unsigned long long bs, unsigned long long localOverlappedIOSize);
+void LocalReadWrite(long long fileSize, DWORD blockSize, int operationsCount, OVERLAPPED* overlappeds, CHAR** buffer, HANDLE fileHandle, char f);
 DWORD LocalDriveSectorSize ();
 DWORD LocalDriveSectorSize (DWORD &localSectorsPerCluster);
-//unsigned long long DWORDS2ULL(DWORD l, DWORD h);
-//void ULL2DWORDS(unsigned long long value, DWORD* l, DWORD* h);
 void CALLBACK CompletionRoutine(DWORD dwErrorCode, DWORD dwNumberOfBytesTransfered, LPOVERLAPPED lpOverlapped);
 
 //Certutil -hashfile file
 
 unsigned long long callback;
-unsigned long long globalFirstAdress;
-unsigned long long oneSize;
-unsigned long long callLeft;
-unsigned long long copyTime = 0;
-
-unsigned long long bs = 20;
-
-void LocalReadWrite(long long fileSize, DWORD blockSize, int operationsCount, OVERLAPPED* overlappeds, CHAR** buffer, HANDLE fileHandle, char f)
-{
-    int operations_counter = 0;
-    for (int i=0; i<operationsCount; i++)
-    {
-        if (fileSize>0)
-        {
-            operations_counter++;
-            if (f == 'r')
-            {
-            	ReadFileEx(fileHandle, buffer[i], blockSize, &overlappeds[i], CompletionRoutine);
-            }
-            else if (f == 'w')
-            {
-            	WriteFileEx(fileHandle, buffer[i], blockSize, &overlappeds[i], CompletionRoutine);
-            }
-            fileSize -= blockSize;
-        }
-    }
-    while (callback < operations_counter)
-    {
-        SleepEx(-1, true);
-    }
-    for (int i=0; i<operationsCount; i++)
-    {
-    	if (f == 'r')
-    	{
-    		overlappeds[i].Offset = shiftRead.LowPart;
-        	overlappeds[i].OffsetHigh = shiftRead.HighPart;
-        	shiftRead.QuadPart += blockSize;
-    	}
-    	else if (f == 'w')
-    	{
-	        overlappeds[i].Offset = shiftWrite.LowPart;
-	        overlappeds[i].OffsetHigh = shiftWrite.HighPart;
-	        shiftWrite.QuadPart += blockSize;
-    	}
-    }
-    callback = 0;
-}
+DWORD copyTime;
+unsigned long long bs = 1;
+unsigned long long oios = 1; // number of operations for Overlapped I/O
 
 // ---------- MAIN ----------
 
 int main(int argc, char **argv)
 {
-    if (argc == 4)
+    if (argc == 6)
     {
-    	const unsigned long oldFileBytes = atoi(argv[2]);
-        const string oldFilePath(argv[1]);
-        const string newFilePath(argv[3]);
+    	const unsigned long oldFileBytes = atoi(argv[2]); // size
+        const string oldFilePath(argv[1]); // old file
+        const string newFilePath(argv[3]); // new file
+        bs = atoi(argv[4]); // multiplitions
+        oios = atoi(argv[5]); // operations
         LocalFileGenerator (oldFilePath, oldFileBytes);
         cout << "Created file number of bytes: " << oldFileBytes << " Created file path: " << oldFilePath << "\n";
         CopyPaste(oldFilePath, newFilePath);
@@ -131,23 +81,16 @@ void CopyPaste (string localOldFilePath, string localNewFilePath)
     DWORD sectorsPerCluster;
     DWORD localSectorSize = LocalDriveSectorSize (sectorsPerCluster);
     unsigned long long localBlockSize; // size of the data block I will copy
-    unsigned long long localOverlappedIOSize; // number of operations for Overlapped I/O
 
     cout << "Drive sector size: " << localSectorSize << " bytes\n";
-    cout << "Drive cluster size: " << sectorsPerCluster << " sectors (" << localSectorSize*sectorsPerCluster << " bytes).\n";
-
-    int localBlockSizeIteration = 0; // iterator
-    
-    localOverlappedIOSize = 1; // overlapped IO number
 
     localBlockSize = localSectorSize*bs;
     copyTime = 0;
-    copyTime = PreparingCopyPaste(localOldFilePath, localNewFilePath, localBlockSize, 1);
-    cout << localBlockSizeIteration << ".\n"
-    << "Current sector size: " << localSectorSize << ";\n"
+    copyTime = PreparingCopyPaste(localOldFilePath, localNewFilePath, localBlockSize, oios);
+    cout << "Current sector size: " << localSectorSize << ";\n"
     << "Current sector multiplition: " << bs << ";\n"
     << "Current block size to copy: " << localBlockSize << ";\n"
-    << "Current overlapped IO number: " << localOverlappedIOSize << ";\n"
+    << "Current overlapped IO number: " << oios << ";\n"
     << "Overlapped copy time: " << copyTime << ".\n";
 }
 
@@ -155,8 +98,6 @@ void CopyPaste (string localOldFilePath, string localNewFilePath)
 
 DWORD PreparingCopyPaste(string localOldFilePath, string localNewFilePath, unsigned long long localBlockSize, unsigned long long localOverlappedIOSize)
 {
-    DWORD localActionTime = -1;
-    
     HANDLE localOldFileHandle = CreateFileA(localOldFilePath.c_str(), GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_FLAG_OVERLAPPED | FILE_FLAG_NO_BUFFERING, NULL); // copied file path
     HANDLE localNewFileHandle = CreateFileA(localNewFilePath.c_str(), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_FLAG_OVERLAPPED | FILE_FLAG_NO_BUFFERING, NULL); // new file path
     DWORD lpFileSizeHigh; // lpdword filesize high
@@ -170,9 +111,8 @@ DWORD PreparingCopyPaste(string localOldFilePath, string localNewFilePath, unsig
     }
     else
     {
-    	DWORD blockSize = 4096*5;
+    	DWORD blockSize = localBlockSize;
         getFileSize = GetFileSize(localOldFileHandle, &lpFileSizeHigh);
-        //fileSize = getFileSize | ((unsigned long long)lpFileSizeHigh << 32);
 
 	    DWORD high = 0;
 	    LARGE_INTEGER fileSizeStruct; // where file size will be wrote
@@ -182,18 +122,19 @@ DWORD PreparingCopyPaste(string localOldFilePath, string localNewFilePath, unsig
 	    long long curSize = fileSize;
 
 	    CHAR** buffer = new CHAR*[localOverlappedIOSize]; // buffer of the data
-	    for (int i=0; i < localOverlappedIOSize; i++)
+	    for (int i = 0; i < localOverlappedIOSize; i++)
 	    {
 	        buffer[i] = new CHAR[(int)blockSize];
 	    }
-	    // äîëæíû îñòàâàòüñÿ äîïóñòèìûìè äëÿ äëèòåëüíîé îïåðàöèè ÷òåíèÿ.
 	    OVERLAPPED* over_1 = new OVERLAPPED[localOverlappedIOSize]; // var for handling pointers (starting bytes)
 	    OVERLAPPED* over_2 = new OVERLAPPED[localOverlappedIOSize]; // new file
 
 	    shiftRead.QuadPart = 0; // how many rode
 	    shiftWrite.QuadPart = 0; // how many write
 
-	    for (int i=0; i < localOverlappedIOSize; i++)
+	    copyTime = GetTickCount(); // time counting
+
+	    for (int i = 0; i < localOverlappedIOSize; i++)
 	    {
 	        over_1[i].Offset = over_2[i].Offset = shiftRead.LowPart; // FIRST PART OF THE STRUCTURE
 	        over_1[i].OffsetHigh = over_2[i].OffsetHigh = shiftRead.HighPart; // SEONDN PART (^$ bit
@@ -212,8 +153,7 @@ DWORD PreparingCopyPaste(string localOldFilePath, string localNewFilePath, unsig
 	    SetFilePointerEx(localNewFileHandle, fileSizeStruct, NULL, FILE_BEGIN);
 	    SetEndOfFile(localNewFileHandle);
 
-        //copyFile(localOldFileHandle, localNewFileHandle, localBlockSize, localOverlappedIOSize);
-        //copyFile(localOldFileHandle, localNewFileHandle, 4096*5, localOverlappedIOSize);
+	    copyTime = GetTickCount() - copyTime; // time counting
     }
 
     // Checking handle and closing the file
@@ -232,49 +172,51 @@ DWORD PreparingCopyPaste(string localOldFilePath, string localNewFilePath, unsig
         }
     }
 
-    return localActionTime;
+    return copyTime;
 }
 
-/*void copyFile(HANDLE firstHandle, HANDLE secondHandle, DWORD blockSize, int operationsCount)
+// ---------- LOCAL READ WRITE ----------
+
+void LocalReadWrite(long long fileSize, DWORD blockSize, int operationsCount, OVERLAPPED* overlappeds, CHAR** buffer, HANDLE fileHandle, char f)
 {
-    DWORD high = 0;
-    LARGE_INTEGER fileSizeStruct; // where file size will be wrote
-    long long fileSize; // number where we write
-    GetFileSizeEx(firstHandle, &fileSizeStruct); // file size (for input as LARGE_INTEGER we use it!!!)
-    fileSize = fileSizeStruct.QuadPart; // uniting fields one number
-    long long curSize = fileSize;
-
-    CHAR** buffer = new CHAR*[operationsCount]; // buffer of the data
-    for (int i=0; i < operationsCount; i++)
+    int operations_counter = 0;
+    for (int i = 0; i < operationsCount; i++)
     {
-        buffer[i] = new CHAR[(int)blockSize];
+        if (fileSize > 0)
+        {
+            operations_counter = operations_counter + 1;
+            if (f == 'r')
+            {
+            	ReadFileEx(fileHandle, buffer[i], blockSize, &overlappeds[i], CompletionRoutine);
+            }
+            else if (f == 'w')
+            {
+            	WriteFileEx(fileHandle, buffer[i], blockSize, &overlappeds[i], CompletionRoutine);
+            }
+            fileSize -= blockSize;
+        }
     }
-    // äîëæíû îñòàâàòüñÿ äîïóñòèìûìè äëÿ äëèòåëüíîé îïåðàöèè ÷òåíèÿ.
-    OVERLAPPED* over_1 = new OVERLAPPED[operationsCount]; // var for handling pointers (starting bytes)
-    OVERLAPPED* over_2 = new OVERLAPPED[operationsCount]; // new file
-
-    shiftRead.QuadPart = 0; // how many rode
-    shiftWrite.QuadPart = 0; // how many write
-
-    for (int i=0; i < operationsCount; i++)
+    while (callback < operations_counter)
     {
-        over_1[i].Offset = over_2[i].Offset = shiftRead.LowPart; // FIRST PART OF THE STRUCTURE
-        over_1[i].OffsetHigh = over_2[i].OffsetHigh = shiftRead.HighPart; // SEONDN PART (^$ bit
-        shiftRead.QuadPart += blockSize;
-        shiftWrite.QuadPart += blockSize;
+        SleepEx(-1, true);
     }
-
-    do
+    for (int i = 0; i < operationsCount; i++)
     {
-    	LocalReadWrite(curSize, blockSize, operationsCount, over_1, buffer, firstHandle, 'r');
-        LocalReadWrite(curSize, blockSize, operationsCount, over_2, buffer, secondHandle, 'w');
-        curSize -= (long long)(blockSize*operationsCount);
+    	if (f == 'r')
+    	{
+    		overlappeds[i].Offset = shiftRead.LowPart;
+        	overlappeds[i].OffsetHigh = shiftRead.HighPart;
+        	shiftRead.QuadPart += blockSize;
+    	}
+    	else if (f == 'w')
+    	{
+	        overlappeds[i].Offset = shiftWrite.LowPart;
+	        overlappeds[i].OffsetHigh = shiftWrite.HighPart;
+	        shiftWrite.QuadPart += blockSize;
+    	}
     }
-    while (curSize > 0);
-
-    SetFilePointerEx(secondHandle, fileSizeStruct, NULL, FILE_BEGIN);
-    SetEndOfFile(secondHandle);
-}*/
+    callback = 0;
+}
 
 // ---------- GET DRIVE SECTOR SIZE ----------
 
